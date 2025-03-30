37150d8b85777ec117a08662d279a24979138b39
kaue_sv <kauesousavieira534@gmail.com>
2025-03-30 14:31:02 -0300

    feat: Criacao do app e mcp do server

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
 from utils.database import seed_vehicles
+from server.mcp_server import MCPServer
 from database.data_source import DatabaseConnection
 from config import Config
 import sys
@@ -18,6 +19,18 @@ def main():
     seed_vehicles.popular_banco()
 
     print(f"Configurado para rodar em: {Config.HOST_SERVER}:{Config.PORT_SERVER}")
+
+    # Iniciando o servidor MCP
+    try:
+        servidor = MCPServer()
+        print("Servidor MCP iniciado. Aguardando conexões...")
+        servidor.iniciar()
+    except KeyboardInterrupt:
+        print("Servidor interrompido pelo usuário.")
+        sys.exit(0)
+    except Exception as e:
+        print(f"Erro ao iniciar o servidor: {e}")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
